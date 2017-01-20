0e149a84eeed08df9a79e885cc8eb456d84045c2
Jakub Arnold <darthdeus@gmail.com>
2017-01-20 01:33:41 +0000

    Migrate to slime + about page

--- config/config.exs
+++ config/config.exs
@@ -17,6 +17,10 @@ config :catsocket_phoenix, CatsocketPhoenix.Endpoint,
   pubsub: [name: CatsocketPhoenix.PubSub,
            adapter: Phoenix.PubSub.PG2]
 
+config :phoenix, :template_engines,
+  slim: PhoenixSlime.Engine,
+  slime: PhoenixSlime.Engine
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
