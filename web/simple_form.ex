e55aad2bc002c4c6f94b517e31df148ea4214790
Jakub Arnold <darthdeus@gmail.com>
2017-02-12 18:29:20 +0100

    Use phoenix's input tag instead of custom ones

--- web/simple_form.ex
+++ web/simple_form.ex
@@ -26,8 +26,8 @@ defmodule Catsocket.SimpleForm do
 
   def input(f, label, name, type) do
     text = case type do
-      :text -> text_input(f, name, class: "form-control")
-      :password -> password_input(f, name, class: "form-control")
+      :text -> Phoenix.HTML.Form.text_input(f, name, class: "form-control")
+      :password -> Phoenix.HTML.Form.password_input(f, name, class: "form-control")
     end
 
     error = case f.source do
@@ -48,17 +48,17 @@ defmodule Catsocket.SimpleForm do
     content_tag :button, text, class: "btn btn-primary", type: "submit"
   end
 
-  def checkbox_input(_f, name, opts) do
-    content_tag :input, "", opts ++ [type: "checkbox", name: name]
-  end
-
-  def text_input(_f, name, opts) do
-    content_tag :input, "", opts ++ [type: "text", name: name]
-  end
-
-  def password_input(_f, name, opts) do
-    content_tag :input, "", opts ++ [type: "password", name: name]
-  end
+  # def checkbox_input(_f, name, opts) do
+  #   content_tag :input, "", opts ++ [type: "checkbox", name: name]
+  # end
+  #
+  # def text_input(_f, name, opts) do
+  #   content_tag :input, "", opts ++ [type: "text", name: name]
+  # end
+  #
+  # def password_input(_f, name, opts) do
+  #   content_tag :input, "", opts ++ [type: "password", name: name]
+  # end
 
   @doc """
   Converts an atom field name to a capitalized label text.
