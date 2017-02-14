f865b4db1351ef6fc1138028b9662e6067c9d6df
Jakub Arnold <darthdeus@gmail.com>
2017-02-14 12:37:14 +0100

    Switching back to brunch

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const jsdir = path.resolve(__dirname, "priv/static/js");
 const HappyPack = require("happypack");
 
 const happyThreadPool = HappyPack.ThreadPool({size: 8});
 
+const jsdir = path.resolve(__dirname, "priv/static/js");
+
 module.exports = {
-  entry: "./web/static/js/app.jsx",
+  entry: [
+    "./web/static/js/app.jsx",
+    "./web/static/js/client.ts"
+  ],
 
   output: {
     path: jsdir,
@@ -14,43 +18,56 @@ module.exports = {
   },
 
   resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
     modules: ["node_modules", jsdir]
   },
 
   module: {
-    loaders: [
-			{
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "happypack/loader?id=woffs",
-        // loader: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-
-			{
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "happypack/loader?id=files"
-        // loader: "file-loader"
-      },
-
-      {
-        test: /\.scss$/,
-        loader: "happypack/loader?id=scss"
-        // loaders: ["style-loader", "css-loader", "resolve-url", "sass-loader"]
-      },
-
+    rules: [
       // {
       //   test: require.resolve('react'),
       //   loader: 'imports'
       // },
 
+			{
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        // loader: "happypack/loader?id=woffs",
+        use: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+
+			{
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        // loader: "happypack/loader?id=files"
+        use: "file-loader"
+      },
+
       {
-        test: /\.(js|jsx)$/,
+        test: /\.scss$/,
+        // loader: "happypack/loader?id=scss"
+        use: ["style-loader", "css-loader", "resolve-url", "sass-loader"]
+      },
+
+      {
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "happypack/loader?id=jsx"
+        use: "happypack/loader?id=jsx"
         // loader: "babel-loader",
         // query: {
         //   presets: ["es2015", "react"]
         // }
-      }
+      },
+
+
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "happypack/loader?id=tsx"
+        // loader: "babel-loader",
+        // query: {
+        //   presets: ["es2015", "react"]
+        // }
+      },
+
     ]
   },
 
@@ -80,6 +97,12 @@ module.exports = {
     new HappyPack({
       id: "jsx",
       loaders: ["babel-loader?cacheDirectory=true&presets[]=es2015&presets[]=react"],
+      threadPool: happyThreadPool,
+    }),
+
+    new HappyPack({
+      id: "tsx",
+      loaders: ["babel-loader?presets[]=react"],
       threadPool: happyThreadPool,
     }),
   ]
