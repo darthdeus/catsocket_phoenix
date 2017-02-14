d27a9b3bedf0fedbe35a5529112dcc7783c6d5fa
Jakub Arnold <darthdeus@gmail.com>
2017-02-14 01:17:51 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Catsocket.Mixfile do
 
   def project do
     [app: :catsocket,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
