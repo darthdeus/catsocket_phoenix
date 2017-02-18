07463b0147ddb7dff8f74508aea85833223fd6c4
Jakub Arnold <darthdeus@gmail.com>
2017-02-18 17:22:25 +0100

    Extract non-websocket logic to ClientHandler

---
+++ lib/catsocket/client_handler.ex
@@ -0,0 +1,165 @@
+defmodule Catsocket.ClientHandler do
+  use GenServer
+
+  alias Catsocket.MessageValidator
+
+  @doc """
+    Starts a new client handler for a given client PID
+  """
+  def start_link(client_pid) do
+    GenServer.start_link(__MODULE__, client_pid, [])
+  end
+
+  @doc """
+    Handles all cleanup for a given client
+  """
+  def closed_connection(pid) do
+    GenServer.call(pid, :closed_connection)
+    :ok
+  end
+
+  @doc """
+    Handles an incoming message
+  """
+  def incoming_message(pid, payload) do
+    GenServer.call(pid, {:incoming_message, payload})
+  end
+
+  ### GenServer callbacks
+
+  def init(client_pid) do
+    initial_state = %{
+      identified: false,
+      api_key:    nil,
+      guid:       nil,
+      client_pid: client_pid
+    }
+
+    {:ok, initial_state}
+  end
+
+  def handle_call(:closed_connection, _from, state) do
+    Catsocket.Sockets.Users.remove(Catsocket.Sockets.Users, self())
+    # TODO: this is wrong, should be removing by client guid
+    Catsocket.Sockets.Rooms.remove_user(Catsocket.Sockets.Rooms, self())
+  end
+
+  def handle_call({:incomming_message, payload}, _from, state) do
+    case MessageValidator.parse(payload) do
+      {:ok, message} ->
+        process_message(message, state)
+
+      {:error, reason} ->
+        {:reply, {:error, reason}, state}
+    end
+  end
+
+  def process_message(message, state) do
+    try do
+      MessageValidator.validate_message(message)
+      MessageValidator.validate_api_key(message["api_key"])
+
+      if Catsocket.Sockets.MessageCache.get(Catsocket.Sockets.MessageCache, message["id"]) do
+        # nic se nedeje, zprava byla zpracovana
+        IO.puts "Message #{inspect message} already processed"
+
+        {:reply, ack(message), state}
+      else
+
+        reply = case message["action"] do
+          "identify"  ->
+            handle_identify(message, state)
+          "join"      ->
+            MessageValidator.validate_identify(state)
+            handle_join(message, state)
+          "leave"     ->
+            MessageValidator.validate_identify(state)
+            handle_leave(message, state)
+          "broadcast" ->
+            MessageValidator.validate_identify(state)
+            handle_broadcast(message, state)
+
+          other -> IO.puts "invalid action #{other}"
+        end
+
+        # Catsocket.MessageCache.put(Catsocket.MessageCache, message["id"])
+        reply
+      end
+    catch
+      {:invalid, attr} ->
+        error = %{error: "Missing attribute '#{attr}'"}
+        json = Poison.encode!(error)
+        {:reply, {:error, json}, state}
+
+      :unidentified ->
+        error = %{error: "Identify required"}
+        json = Poison.encode!(error)
+        {:reply, {:error, json}, state}
+
+      :wrong_api_key ->
+        error = %{error: "Api key required"}
+        json = Poison.encode!(error)
+        {:reply, {:error, json}, state}
+    end
+  end
+
+  ## Client action handlers
+
+  defp handle_identify(message, state) do
+    Catsocket.Sockets.Users.associate(Catsocket.Sockets.Users, message["user"], self())
+
+    new_state = %{state | api_key: message["api_key"], guid: message["user"], identified: true}
+
+    {:reply, {:ok, ack(message)}, new_state}
+  end
+
+  defp handle_join(message, state) do
+    if ! Map.has_key?(message["data"], "room"), do: throw {:invalid, "room"}
+
+    # IO.puts "joined"
+    room = message["data"]["room"]
+
+    Catsocket.Sockets.Rooms.join(Catsocket.Sockets.Rooms, message["api_key"], room, state[:guid])
+
+    {:reply, {:ok, ack(message)}, state}
+  end
+
+  defp handle_leave(message, state) do
+    if ! Map.has_key?(message["data"], "room"), do: throw {:invalid, "room"}
+
+    room = message["data"]["room"]
+
+    Catsocket.Sockets.Rooms.leave(Catsocket.Sockets.Rooms, message["api_key"], room, state[:guid])
+
+    {:reply, {:ok, ack(message)}, state}
+  end
+
+  defp handle_broadcast(message, state) do
+    # TODO - validate message length
+    if ! Map.has_key?(message["data"], "room"), do: throw {:invalid, "room"}
+    if ! Map.has_key?(message["data"], "message"), do: throw {:invalid, "message"}
+
+    room = message["data"]["room"]
+    text = message["data"]["message"]
+    Catsocket.Sockets.Rooms.broadcast(Catsocket.Sockets.Rooms, message["api_key"], room, text)
+
+    {:reply, {:ok, ack(message)}, state}
+  end
+
+  ## Response helpers
+
+  def ack(message) do
+    json = Poison.encode!(build_ack(message))
+    {:text, json}
+  end
+
+  defp build_ack(message) do
+    %{
+      id: message["id"],
+      data: %{},
+      action: "ack",
+      timestamp: message["timestamp"]
+    }
+  end
+
+end
---
+++ lib/catsocket/message_validator.ex
@@ -0,0 +1,33 @@
+defmodule Catsocket.MessageValidator do
+  def validate_message(message) do
+    if ! Map.has_key?(message, "api_key"),   do: throw {:invalid, "api_key"}
+    if ! Map.has_key?(message, "user"),      do: throw {:invalid, "user"}
+    if ! Map.has_key?(message, "id"),        do: throw {:invalid, "id"}
+    if ! Map.has_key?(message, "timestamp"), do: throw {:invalid, "timestamp"}
+    if ! Map.has_key?(message, "action"),    do: throw {:invalid, "action"}
+    if ! Map.has_key?(message, "data"),      do: throw {:invalid, "data"}
+  end
+
+  def validate_identify(state) do
+    if ! state[:identified], do: throw :unidentified
+  end
+
+  def validate_api_key(_key) do
+    #  if ! Catsocket.Keys.get(Catsocket.Keys, key), do: throw :wrong_api_key
+  end
+
+  def parse(payload) do
+    case Poison.decode(payload) do
+      {:ok, message} ->
+        MessageValidator.is_valid_message(message)
+        if is_map(message) do
+          {:ok, message}
+        else
+          {:error, "received something that wasn't a map: #{inspect message}"}
+        end
+
+      {:error, reason} ->
+        {:error, "received invalid json: #{reason}"}
+    end
+  end
+end
