d5469918ed5a0a69de4bd4da3c04b192f09b50e2
Jakub Arnold <darthdeus@gmail.com>
2017-02-19 16:02:07 +0100

    Version bump to 0.1.0, binary protocol \o/

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Catsocket.Mixfile do
 
   def project do
     [app: :catsocket,
-     version: "0.0.3",
+     version: "0.1.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
